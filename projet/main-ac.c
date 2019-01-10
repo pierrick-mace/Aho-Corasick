ba428c9ba9283f04256f9c66b19a8e10c135ed8d
Pierrick <pi.mace@free.fr>
2019-01-10 15:44:11 +0100

    modifs main-ac

--- projet/main-ac.c
+++ projet/main-ac.c
@@ -1,45 +1,47 @@
+// #define _XOPEN_SOURCE 700
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <string.h>
 #include "trie.h"
 #include "ac.h"
 
 #define BUFFSIZE_INIT 64
 
-int _insertWords(Trie trie, FILE *file);
+static int insertWordsInTrie(Trie trie, FILE *file);
 int _readLine(FILE *file, char **buff, size_t *buffSize);
+static int getFileSize(FILE *file);
 
 int main(int argc, char **argv) {
     if (argc != 3) {
-        fprintf(stderr, "usage: %s mots.txt texte.txt\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    FILE *mots = fopen(argv[1], "r");
-    if (mots == NULL) {
-        fprintf(stderr, "impossible d'ouvrir le fichier \"%s\""
-            " en lecture\n", argv[1]);
+        fprintf(stderr, "[USAGE]: %s words text\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    fseek(mots, 0, SEEK_END);
-    int taille_mots = (int) ftell(mots);
-    rewind(mots);
+    FILE *words = fopen(argv[1], "r");
+    if (words == NULL) {
+        fprintf(stderr, "Error: failed to open file %s", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-    Trie trie = createTrie(taille_mots);
+    int size = getFileSize(words);
+
+    Trie trie = createTrie(size);
     if (trie == NULL) {
-        fclose(mots);
-        fprintf(stderr, "erreur lors de la création du Trie\n");
+        fclose(words);
+        fprintf(stderr, "Error: couldn't create trie\n");
         return EXIT_FAILURE;
     }
 
-    if (_insertWords(trie, mots) == -1) {
+    if (insertWordsInTrie(trie, words) == -1) {
         freeTrie(trie);
-        fclose(mots);
+        fclose(words);
         fprintf(stderr, "erreur lors de l'insertion des mots dans le Trie\n");
         return EXIT_FAILURE;
     }
 
-    fclose(mots);
+    fclose(words);
 
     AC ac = createAC(trie);
     if (ac == NULL) {
@@ -49,7 +51,7 @@ int main(int argc, char **argv) {
     }
 
     FILE *texte = fopen(argv[2], "r");
-    if (mots == NULL) {
+    if (texte == NULL) {
         freeTrie(trie);
         freeAC(ac);
         fprintf(stderr, "impossible d'ouvrir le fichier \"%s\""
@@ -65,45 +67,31 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
-int _insertWords(Trie trie, FILE *file) {
-    size_t buffSize = BUFFSIZE_INIT;
-    char *buff = malloc(BUFFSIZE_INIT);
-    if (buff == NULL) {
-        return -1;
+static int insertWordsInTrie(Trie trie, FILE *file) {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, file)) != -1) {
+      if ((line)[read - 1] == '\n') {
+          (line)[read - 1] = '\0';
+          --read;
+      }
+
+      insertInTrie(trie, (unsigned char *) line);
     }
 
-    while (_readLine(file, &buff, &buffSize) != -1) {
-        insertInTrie(trie, (unsigned char *) buff);
-    }
-
-    free(buff);
     return 0;
 }
 
-int _readLine(FILE *file, char **buff, size_t *buffSize) {
-    char *s = fgets(*buff, (int) *buffSize, file);
-    if (s == NULL) {
-        return -1;
-    }
-    size_t len = strlen(*buff);
-    char end = (*buff)[len - 1];
-    while (end != '\n') {
-        *buffSize *= 2;
-        s = realloc(*buff, *buffSize);
-        if (s == NULL) {
-            return -1;
-        }
+static int getFileSize(FILE *file) {
+  if (file == NULL) {
+    return -1;
+  }
 
-        s = fgets(*buff + len, (int) *buffSize / 2, file);
-        if (s == NULL) {
-            return *(*buff + len) == '\0' ? -1 : 0;
-        }
+  fseek(file, 0, SEEK_END);
+  int size = (int) ftell(file);
+  rewind(file);
 
-        len = strlen(*buff);
-        end = (*buff)[len - 1];
-    }
-
-    (*buff)[len - 1] = '\0';
-
-    return 0;
+  return size;
 }
